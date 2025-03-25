cacfcd0af80a3676cfa77c8ee8a34f636ef5ce43
TanguyDubois <dubois.tanguy@gmail.com>
2025-03-25 18:02:49 +0100

    tout marche !

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ fn main() {
     let crashtest_program: Program = vec![
         Instruction::Control(ControlASM::Mov(1.into(), var.clone())),
         Instruction::Effect(
-            Event::MidiNote(60.into(), 90.into(), 0.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
+            Event::MidiNote(60.into(), 90.into(), 1.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
             TimeSpan::Micros(1_000_000).into(),
         ),
         Instruction::Control(ControlASM::Sub(var.clone(), 1.into(), var.clone())),
@@ -85,24 +85,24 @@ fn main() {
     let crashtest_program_with_calls: Program = vec![
         Instruction::Control(ControlASM::CallProcedure(6)),
         Instruction::Effect(
-            Event::MidiNote(100.into(), 90.into(), 0.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
+            Event::MidiNote(80.into(), 90.into(), 1.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
             TimeSpan::Micros(100).into(),
         ),
         Instruction::Control(ControlASM::CallProcedure(6)),
         Instruction::Effect(
-            Event::MidiNote(102.into(), 90.into(), 0.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
+            Event::MidiNote(82.into(), 90.into(), 1.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
             TimeSpan::Micros(100).into(),
         ),
         Instruction::Control(ControlASM::CallProcedure(9)),
         Instruction::Control(ControlASM::Return),
         Instruction::Effect(
-            Event::MidiNote(104.into(), 90.into(), 0.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
+            Event::MidiNote(84.into(), 90.into(), 1.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
             TimeSpan::Micros(100).into(),
         ),
         Instruction::Control(ControlASM::CallProcedure(9)),
         Instruction::Control(ControlASM::Return),
         Instruction::Effect(
-            Event::MidiNote(106.into(), 90.into(), 0.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
+            Event::MidiNote(86.into(), 90.into(), 1.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
             TimeSpan::Micros(100).into(),
         ),
         Instruction::Control(ControlASM::Return),
@@ -110,7 +110,7 @@ fn main() {
 
     let crashtest_func: Program = vec![
         Instruction::Effect(
-            Event::MidiNote(40.into(), 90.into(), 0.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
+            Event::MidiNote(40.into(), 90.into(), 1.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
             TimeSpan::Micros(500000).into(),
         ),
         Instruction::Control(ControlASM::Return),
@@ -120,7 +120,7 @@ fn main() {
         Instruction::Control(ControlASM::Mov(Variable::Constant(VariableValue::Func(crashtest_func.clone())), var.clone())),
         Instruction::Control(ControlASM::CallFunction(var.clone())),
         Instruction::Effect(
-            Event::MidiNote(501.into(), 90.into(), 0.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
+            Event::MidiNote(67.into(), 90.into(), 1.into(), TimeSpan::Micros(500_000).into(), midi_name.clone().into()),
             TimeSpan::Micros(100).into(),
         ),
         Instruction::Control(ControlASM::Return),
@@ -129,18 +129,23 @@ fn main() {
 
     // This is a test program obtained from a script
     let crashtest_parsed_program: Program = dummy
-        .compile("N 40 2 1")
+        .compile("N 100 2 1")
         .unwrap();
-    //print!("{:?}", crashtest_parsed_program);
+    print!("{:?}", crashtest_parsed_program);
 
     let mut sequence1 = Sequence::new(vec![1.0]);
     let mut sequence2 = Sequence::new(vec![1.0,1.0]);
     let mut sequence3 = Sequence::new(vec![0.25]);
+    let mut sequence4 = Sequence::new(vec![1.0,1.0,1.0,1.0]);
     sequence1.set_script(0, kick.clone().into());
     sequence2.set_script(1, tom.clone().into());
     sequence3.set_script(0, hihats.clone().into());
+    sequence4.set_script(0, crashtest_program.into());
+    sequence4.set_script(1, crashtest_program_with_calls.into());
+    sequence4.set_script(2, crashtest_program_with_function_calls.into());
+    sequence4.set_script(3, crashtest_parsed_program.into());
     let pattern = Pattern {
-        sequences: vec![sequence1, sequence2, sequence3],
+        sequences: vec![sequence1, sequence2, sequence3, sequence4],
     };
     let message = SchedulerMessage::UploadPattern(pattern);
     let _ = sched_iface.send(message);
