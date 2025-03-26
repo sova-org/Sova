b576d3fd7cc3bf1bf2f111c331c36592f2dc693c
TanguyDubois <dubois.tanguy@gmail.com>
2025-03-26 17:45:19 +0100

    TCP server, no processing yet

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
-use std::{collections::HashMap, sync::Arc, thread, time::Duration, vec};
+use std::sync::Arc;
 use crate::clock::ClockServer;
-use crate::pattern::Pattern;
-use clock::TimeSpan;
-use compiler::{
-    dummylang::DummyCompiler,
-    Compiler,
-    ExternalCompiler
-};
+
 use device_map::DeviceMap;
-use lang::{
-    control_asm::ControlASM,
-    event::Event,
-    variable::{Variable, VariableValue},
-    Instruction, Program
-};
-use pattern::{script::Script, Sequence};
+
 use protocol::midi::{MidiInterface, MidiOut};
-use schedule::{Scheduler, SchedulerMessage};
+use schedule::Scheduler;
+use server::BuboCoreServer;
 use world::World;
-use tokio::signal;
 
 pub mod clock;
 pub mod compiler;
@@ -29,6 +17,8 @@ pub mod pattern;
 pub mod protocol;
 pub mod schedule;
 pub mod world;
+
+pub mod server;
 
 #[tokio::main]
 async fn main() {
@@ -46,24 +36,13 @@ async fn main() {
     let (sched_handle, sched_iface) =
         Scheduler::create(clock_server.clone(), devices.clone(), world_iface.clone());
 
-    let _bete = ExternalCompiler("bete".to_owned());
-    let dummy = DummyCompiler;
+    let mut server = BuboCoreServer { ip: "127.0.0.1".to_owned(), port: 8080 };
+    server.start().await.expect("Server failed");
 
-    println!("Waiting for interrupt");
-    signal::ctrl_c().await.expect("failed to listen for event");;
     println!("\n[-] Stopping BuboCore...");
     drop(sched_iface);
     drop(world_iface);
     sched_handle.join().expect("Scheduler thread error");
     world_handle.join().expect("World thread error");
-}
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
 }
