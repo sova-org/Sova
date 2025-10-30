6f40f78fb862545b153504f112858e0fbeda44ad
Tanguy Dubois <dubois.tanguy@gmail.com>
2025-10-30 22:04:17 +0100

    working on solo tui

--- solo-tui/src/main.rs
+++ solo-tui/src/main.rs
@@ -1,13 +1,48 @@
+use std::sync::Arc;
+
+use sova_core::{clock::ClockServer, compiler::{bali::BaliCompiler, dummylang::DummyCompiler, ExternalCompiler}, device_map::DeviceMap, init, lang::{interpreter::{boinx::BoinxInterpreterFactory, external::ExternalInterpreterFactory, InterpreterDirectory}, LanguageCenter, Transcoder}, schedule::SchedulerMessage};
+
 use crate::app::App;
 
 pub mod app;
 pub mod event;
 pub mod ui;
 
+const DEFAULT_TEMPO : f64 = 120.0;
+const DEFAULT_QUANTUM : f64 = 4.0;
+const DEFAULT_MIDI_OUT : &str = "SovaOut";
+
+fn create_language_center() -> Arc<LanguageCenter> {
+    let mut transcoder = Transcoder::default();
+    transcoder.add_compiler(BaliCompiler);
+    transcoder.add_compiler(DummyCompiler);
+    transcoder.add_compiler(ExternalCompiler);
+    let mut interpreters = InterpreterDirectory::new();
+    interpreters.add_factory(BoinxInterpreterFactory);
+    interpreters.add_factory(ExternalInterpreterFactory);
+    Arc::new(LanguageCenter { transcoder, interpreters })
+}
+
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
-    let result = App::new().run(terminal);
+
+    let clock_server = Arc::new(ClockServer::new(DEFAULT_TEMPO, DEFAULT_QUANTUM));
+    let languages = create_language_center();
+    let devices = Arc::new(DeviceMap::new());
+
+    let _ = devices.create_virtual_midi_port(DEFAULT_MIDI_OUT);
+
+    let (world_handle, sched_handle, sched_iface, sched_updates) = 
+        init::start_scheduler_and_world(clock_server, devices.clone(), languages);
+
+    let result = App::new(sched_iface.clone(), sched_updates).run(terminal);
     ratatui::restore();
+
+    devices.panic_all_midi_outputs();
+    let _ = sched_iface.send(SchedulerMessage::Shutdown);
+    let _ = world_handle.join();
+    let _ = sched_handle.join();
+
     result
 }
