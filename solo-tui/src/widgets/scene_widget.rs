181a27173ccb8937a06fd844c2785a92d98ad7ba
Tanguy Dubois <dubois.tanguy@gmail.com>
2025-11-02 00:36:19 +0100

    Scene widget beginning

--- solo-tui/src/widgets/scene_widget.rs
+++ solo-tui/src/widgets/scene_widget.rs
@@ -1,14 +1,16 @@
 use ratatui::{buffer::Buffer, layout::Rect, widgets::StatefulWidget};
 
-use crate::{app::AppState, event::EventHandler};
+use crate::app::AppState;
 
 pub struct SceneWidget {}
 
-impl StatefulWidget for SceneWidget {
+impl StatefulWidget for &SceneWidget {
     type State = AppState;
 
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
         let structure = state.scene_image.structure();
+
+        let cols = state.scene_image.n_lines();
 
         todo!()
     }
