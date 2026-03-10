c400cfbb47e21acf1b5999e27d0a279b0418c6a0
Tanguy Dubois <dubois.tanguy@gmail.com>
2026-03-10 16:00:00 +0100

    Testing untagged variableValues

--- core/src/vm/variable.rs
+++ core/src/vm/variable.rs
@@ -22,12 +22,19 @@ pub enum VariableValue {
     Func(Program),
     Blob(Vec<u8>),
     Generator(ValueGenerator),
+    #[serde(untagged)]
     Integer(i64),
+    #[serde(untagged)]
     Float(f64),
+    #[serde(untagged)]
     Bool(bool),
+    #[serde(untagged)]
     Str(String),
+    #[serde(untagged)]
     Dur(TimeSpan),
+    #[serde(untagged)]
     Map(HashMap<String, VariableValue>),
+    #[serde(untagged)]
     Vec(Vec<VariableValue>),
 }
 
